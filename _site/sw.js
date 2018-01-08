c03be2f9207f72f638d7890a320e2631ba90927e
joshvogt <josh.vogt@workopolis.com>
2018-01-08 11:26:36 -0500

    Actually fixing the SW

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-var cacheName = 'proto1.1';
-var dataCacheName = 'proto1.1';
+var cacheName = 'proto1.2';
+var dataCacheName = 'proto1.2';
 
 var getPageURL = location;
 
@@ -7,35 +7,39 @@ var filesToCache = [
  '/manifest.json',
  '/static-assets/js/home.js',
  '/static-assets/css/new-base.css',
- '/static-assets/js/modernizr_grid.js',
  '/index.html',
+ '/',
  '404.html'
 ];
 
 
 self.addEventListener('install', function(e) {
-    // console.log('[ServiceWorker] Install');
-    // debugger;
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            // console.log('[ServiceWorker] Caching app shell');
+            //console.log('adding to cache');
             return cache.addAll(filesToCache);
         }).then(function(){
          return self.skipWaiting();
+         //console.log('here i am');
         })
     );
 });
 
 self.addEventListener('activate', function(e) {
-    // console.log('[ServiceWorker] Activate');
+    //console.log('[ServiceWorker] Activate');
     e.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName && key !== dataCacheName) {
-                    // console.log('[ServiceWorker] Removing old cache', key);
+                if (key !== cacheName || key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
+        }).then(function(){
+            caches.open(cacheName).then(function(cache) {
+                //console.log('adding to cache');
+                return cache.addAll(filesToCache);
+            })
         })
     );
     return self.clients.claim();
@@ -44,16 +48,9 @@ self.addEventListener('activate', function(e) {
 
 
 self.addEventListener('fetch', function(e) {
-    // console.log('[Service Worker] Fetch', e.request.url);
+    //console.log('[Service Worker] Fetch', e.request.url);
     var dataUrl = '/old/en/';
     if (e.request.url.indexOf(dataUrl) > -1) {
-        /*
-        * When the request URL contains dataUrl, the app is asking for fresh
-        * weather data. In this case, the service worker always goes to the
-        * network and then caches the response. This is called the "Cache then
-        * network" strategy:
-        * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-        */
         e.respondWith(
             caches.open(dataCacheName).then(function(cache) {
                 return fetch(e.request).then(function(response){
@@ -63,11 +60,7 @@ self.addEventListener('fetch', function(e) {
             })
         );
     } else {
-        /*
-        * The app is asking for app shell files. In this scenario the app uses the
-        * "Cache, falling back to the network" offline strategy:
-        * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-        */
+        //console.log('what happened');
         e.respondWith(
             caches.match(e.request).then(function(response) {
                 return response || fetch(e.request);
