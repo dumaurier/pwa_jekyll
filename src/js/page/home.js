0ab761ded48e5d773b66da6313f2a7b020f63c23
joshvogt <josh.vogt@workopolis.com>
2018-01-10 09:14:41 -0500

    Placeholder JS

--- src/js/page/home.js
+++ src/js/page/home.js
@@ -1,1 +1,20 @@
-console.log('hello');
+var JekyllPWA = JekyllPWA || {};
+
+JekyllPWA.Home = {
+    init: function () {
+        this.initHello();
+        this.initAnotherFunction();
+    },
+
+    initHello: function () {
+        console.log('hello.');
+    },
+
+    initAnotherFunction: function() {
+      // this function does nothing. Do something with it if you want.
+    }
+};
+
+(function () {
+    JekyllPWA.Home.init();
+})();
