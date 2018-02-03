e9332cc8cda3ac3d0d372890e5a7fd9812c70214
Josh Vogt <joshvogt@gmail.com>
2018-02-03 12:00:26 -0500

    Yeesh, this is goofy

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'sw-1517677077';
-const dataCacheName = 'sw-1517677077';
+const cacheName = 'sw-1517677211';
+const dataCacheName = 'sw-1517677211';
 const RUNTIME = 'runtime';
 
 var getPageURL = location;
@@ -49,7 +49,7 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin) && !event.request.url.endsWith('.js') && !event.request.url.endsWith('token') && !event.request.url.endsWith('recover') && !event.request.url.endsWith('verify')) {
+  if (event.request.url.startsWith(self.location.origin) && !event.request.url.endsWith('.js') && !event.request.url.endsWith('.netlify/**')) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
