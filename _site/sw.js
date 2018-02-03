ff3fe7fb0c71470a06b1eccd5e172fed6521c792
Josh Vogt <joshvogt@gmail.com>
2018-02-03 12:20:43 -0500

    I'm not having fun anymore

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'sw-1517678143';
-const dataCacheName = 'sw-1517678143';
+const cacheName = 'sw-1517678433';
+const dataCacheName = 'sw-1517678433';
 const RUNTIME = 'runtime';
 
 var getPageURL = location;
@@ -49,7 +49,7 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin) && !event.request.url.endsWith('.js') && !event.request.url.endsWith('token') && !event.request.url.endsWith('recover') && !event.request.url.endsWith('verify') && !event.request.url.endsWith('user')) {
+  if (event.request.url.startsWith(self.location.origin) && !event.request.url.endsWith('.js') && !event.request.url.startsWith(self.location.origin + '.netlify/')) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
