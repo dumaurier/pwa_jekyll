48a26b3ddd1cba24cd892419bd991f0878c0d62e
Josh Vogt <joshvogt@gmail.com>
2018-02-04 22:10:57 -0500

    Landing pages for post types
    Also, some fun with CSS Grid

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'sw-1517755411';
-const dataCacheName = 'sw-1517755411';
+const cacheName = 'sw-1517800204';
+const dataCacheName = 'sw-1517800204';
 const RUNTIME = 'runtime';
 
 var getPageURL = location;
