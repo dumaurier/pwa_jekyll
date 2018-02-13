84ea6d7e1c791469e9a9522503df3808e0b9bfd7
Josh Vogt <joshvogt@gmail.com>
2018-02-12 22:55:07 -0500

    Fixing some broken things in IDB structure
    This will be the last time I carelessy check into Master. Time to start
    working on a branch.

--- src/js/page/posts.js
+++ src/js/page/posts.js
@@ -1,48 +1,23 @@
 var JekyllPWA = JekyllPWA || {};
 var mentions = [];
 
+//Things we need
+const page = document.getElementById('page-path').value;
+const webmention = "https://webmention.io/api/mentions?target=";
+const pageID = document.getElementById('page-id').value; //md5 hash of {{ page.id }} - needed for indexedDB Store
+const request = webmention + page;
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+
+
 JekyllPWA.Posts = {
     init: function () {
-        this.initDisplayMentions();
         this.initCreateMentionsStore();
-        this.initStoreMentions();
-        
-    },
-
-    initDisplayMentions: function () {
-        const page = document.getElementById('page-path').value;
-        const webmention = "https://webmention.io/api/mentions?target=";
-        const request = webmention + page;
-
-            fetch(request)
-            .then(response => response.json())
-            .then(data => {
-                if(data.links.length == 0){
-                console.log('no mentions yet');
-                }
-                else{
-                document.querySelector('.post-webmentions').removeAttribute('hidden');
-                data.links.forEach(function(mention){
-                    var tmpl = document.getElementById('mention-tmpl').content.cloneNode(true);
-
-                    tmpl.querySelector('.mention-reply-content').innerHTML = mention.data.content;
-                    tmpl.querySelector('.mention-reply-author').innerText = mention.data.author.name;
-                    tmpl.querySelector('.mention-reply-link').setAttribute('href',mention.source);
-                    tmpl.querySelector('.mention-reply-link').innerText = mention.data.author.url;
-                    document.getElementById('webmentions-container').appendChild(tmpl);
-                    mentions.push({id: mention.id, source: mention.source, target: mention.target, content: mention.data.content, author: mention.data.author.name, url: mention.data.author.url});
-                });
-                }
-                console.log(data)
-            });
+        //this.initCheckForStoredMentions();
+        this.initDisplayMentions();
     },
 
     initCreateMentionsStore: function() {
-        //Thing we'll need
-        const pageID = document.getElementById('page-id').value; //md5 hash of {{ page.id }} - needed for indexedDB Store
-
-        let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-
         // Create IndexedDB Store for each article. 
         var open = indexedDB.open("Mentions", 1);
         // Create the schema
@@ -50,33 +25,79 @@ JekyllPWA.Posts = {
         let db = open.result;
 
         //Key path is the MD5 hash of the value of the {{ post.id }} variable in Jekyll. 
-        const mentionStore = db.createObjectStore(pageID, {keyPath: "id"});
-        const mentionIndex = store.createIndex(pageID + "-index", "id");
+        var mentionStore = db.createObjectStore(pageID, {keyPath: "id"});
+        var index = mentionStore.createIndex("mentions", "id");
         };
     },
 
-    initStoreMentions: function() {
-        const pageID = document.getElementById('page-id').value; //md5 hash of {{ page.id }} - needed for indexedDB Store
+    initCheckForStoredMentions: function() {
+        var openStorage = window.indexedDB.open( "Mentions", 1 );
+        
+        openStorage.onsuccess = function(event){
+            db = openStorage.result;
 
-        mentions.forEach(function(el){
-            var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-            var openStorage = window.indexedDB.open("Mentions", 1);
+            var transaction = db.transaction([ pageID ], "readwrite" );
+            var objectStore = transaction.objectStore( pageID );
+            var objectStoreRequest = objectStore.getAll();
 
-            openStorage.onsuccess = function(event){
-                db = openStorage.result;
-                var transaction = db.transaction([pageID], "readwrite");
-                var objectStore = transaction.objectStore(pageID);
-    
-                transaction = db.transaction([pageID], "readwrite");
-                objectStore = transaction.objectStore(pageID);
-                objectStoreRequest = objectStore.put({id: el.id, source: el.source, target: el.target, content: el.content, author: el.author, url: el.author});
-                objectStoreRequest.oncomplete = function (e) {
-                    console.log("asd");
+            objectStoreRequest.onsuccess = function(event) {
+                var mentionsOffline = objectStoreRequest.result;
+                
+                if (mentionsOffline.length >= mentions.length) {
+                    mentions = [];
+                    mentionsOffline.forEach(function(mention) {
+                        mentions.push({id: mention.id, source: mention.source, target: mention.target, content: mention.content, author: mention.author, url: mention.url, fromID: "yeah"});
+                    });
                 }
+            };
+        };
+    },
+
+    initDisplayMentions: function () {
+        fetch(request)
+        .then(response => response.json())
+        .then(data => {
+            if(data.links.length == 0){
+                console.log('no mentions yet');
+            }
+            else{
+                //show the mentions container
+                document.querySelector('.post-webmentions').removeAttribute('hidden');
+                data.links.forEach(function(mention){
+
+                    // render the template
+                    var tmpl = document.getElementById('mention-tmpl').content.cloneNode(true);
+                    tmpl.querySelector('.mention-reply-content').innerHTML = mention.data.content;
+                    tmpl.querySelector('.mention-reply-author').innerText = mention.data.author.name;
+                    tmpl.querySelector('.mention-reply-link').setAttribute('href',mention.source);
+                    tmpl.querySelector('.mention-reply-link').innerText = mention.data.author.url;
+                    document.getElementById('webmentions-container').appendChild(tmpl);
+
+                    // push the data we want from the request into the array
+                    mentions.push({id: mention.id, source: mention.source, target: mention.target, content: mention.data.content, author: mention.data.author.name, url: mention.data.author.url});
+                });
             }
         })
-
-    }
+        .then(function(){
+            mentions.forEach(function(el){
+                var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+                var openStorage = window.indexedDB.open("Mentions", 1);
+        
+                openStorage.onsuccess = function(event){
+                    db = openStorage.result;
+                    var transaction = db.transaction([pageID], "readwrite");
+                    var objectStore = transaction.objectStore(pageID);
+            
+                    transaction = db.transaction([pageID], "readwrite");
+                    objectStore = transaction.objectStore(pageID);
+                    objectStoreRequest = objectStore.put({id: el.id, source: el.source, target: el.target, content: el.content, author: el.author, url: el.author});
+                    objectStoreRequest.oncomplete = function (e) {
+                        console.log("asd");
+                    }
+                }
+            })
+        });
+    }    
 };
 
 (function () {
